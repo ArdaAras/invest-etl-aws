12745261bd2c2eb1f6b72d9576f25dbb91678157
Arda <ardaras3@gmail.com>
2023-09-21 22:20:57 +0300

    Driver initial and Infra update
    + Cloudwatch rule added (daily at 10 PM)
    + driver.py added for infra testing
    + REGION added to config file

---
+++ Source/driver.py
@@ -0,0 +1,3 @@
+import infrastructure as infra
+
+infra.create_cloudwatch_rule()
--- Source/infrastructure.py
+++ Source/infrastructure.py
@@ -1,11 +1,28 @@
 import configparser
 import os
+import boto3
 
 config = configparser.ConfigParser()
-config.read('config.cfg')
+config.read('Source\config.cfg')
 
 os.environ['AWS_ACCESS_KEY_ID'] = config['KEYS']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY'] = config['KEYS']['AWS_SECRET_ACCESS_KEY']
+region_name = config['REGION']['REGION_NAME']
+
+def create_cloudwatch_rule():
+    # Initialize the CloudWatch client
+    cloudwatch = boto3.client('events',region_name=region_name)
+
+    # Define the rule name and schedule expression
+    rule_name = 'DailyAt10PM'
+    schedule_expression = 'cron(0 22 * * ? *)' # 10 PM which is after NASDAQ market closes for the day
+
+    # Create the CloudWatch Events rule
+    response = cloudwatch.put_rule(
+        Name=rule_name,
+        ScheduleExpression=schedule_expression,
+        State='ENABLED'  # Set to 'ENABLED' to enable the rule immediately
+    )
 
 def create_iam_role():
     pass
